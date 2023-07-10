a990842d6a92954e347c1417a411a12db7b7b03b
= <=>
2023-07-09 23:21:31 -0400

    prints sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a = 0
+    b = 1
+    fib = []
+    for i in range(length):
+        fib.append(a)
+        a, b = b, a + b
+
+    print(fib)
+
+
+print_fibonacci(10)
